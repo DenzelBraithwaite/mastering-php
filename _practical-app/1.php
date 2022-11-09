8d323126c30621346b62bc1aab4bc3f6fc4dd659
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-11-09 08:01:32 -0500

    Completed challenge 1

--- _practical-app/1.php
+++ _practical-app/1.php
@@ -17,19 +17,12 @@
 
 		<?php  
 
-
-		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
+			// Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
+			// Step 2: Write a comment above the echo function and explain what that function is doing.
 			
+			// Prints hello
+			echo "<h1>hello</h1>";	
 
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
-
-
-		   */
-
-		  // Says hi
-		  echo "<h1>Hello</h1>"
 		?>
 	
 
