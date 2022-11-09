11113c117439835eae709bc4c1e1f4c6a2aaae69
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-11-09 07:38:21 -0500

    Completed exercise 3

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -14,17 +14,38 @@
 
 <?php  
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+// Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
+if (5 < 4) {
+	echo "t";
+} elseif (5 < 3) {
+	echo "nun";
+} else {
+	echo "I love PHP";
+}
+echo "<br>";
+// Step 2: Make a forloop  that displays 10 numbers
+for ($i = 10; $i <= 100; $i += 10) {
+	echo "{$i}<br>";
+}
 
-
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
- */
-
+// Step 3 : Make a switch Statement that test againts one condition with 5 cases
+switch (10) {
+	case 2:
+		echo "2";
+		break;
+	case 4:
+		echo "4";
+		break;
+	case 6:
+		echo "6";
+		break;
+	case 8:
+		echo "8";
+		break;
+	case 10:
+		echo "10";
+}
 	
 ?>
 
