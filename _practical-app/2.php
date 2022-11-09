8fcb3b5329ffdf6cb4b5bd59c3b5efb7536afef3
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-11-09 07:59:12 -0500

    Completed challenge 2

--- _practical-app/2.php
+++ _practical-app/2.php
@@ -17,22 +17,21 @@
 
 		<?php
 
-		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+			// Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+			$number1 = 10;
+			$number2 = 20;
 
-		  Step 2: Add the two variables and display the sum with echo:
+			// Step 2: Add the two variables and display the sum with echo:
+			echo $number1 + $number2 . "<br>";
 
+			// Step 3: Make 2 Arrays with the same values, one regular and the other associative
+			$array = [1, 2, 3];
+			$associative = array(0 => 1, 1 => 2, 2 => 3);
+			echo $associative[2];
 
-		  Step3: Make 2 Arrays with the same values, one regular and the other associative
-
-		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
-
-
-			
-
-			 */
-
-		
-
+			// Step 4: Make a constant and set it to the value of PHP. and use an echo to print it out
+			define("CONST", "PHP");
+			echo CONST;
 
 		?>
 
