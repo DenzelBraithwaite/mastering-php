ab47891079e7a0fe7d89845c26282daf4494f5e8
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-12-11 14:07:47 -0500

    added validation message for username

--- _practical-app/6.php
+++ _practical-app/6.php
@@ -13,25 +13,31 @@
 
 <article class="main-content col-xs-8">
 
-	<form action="test_form.php" method="POST">
-		<label for="username">Username</label>
-		<input name="username" type="text" placeholder="Ex: Kaz123">
-		<br>
-		<label for="password">Password</label>
-		<input name="password" type="password">
-		<br>
-		<button name="submit" type="submit">Submit</button>
-	</form>
-
-	<?php  
-
-/*  Step1: Make a form that submits one value to POST super global
-
-
- */
 	
-?>
+<!-- Step1: Make a form that submits one value to POST super global -->
+	<?php  
+		if (isset($_POST['submit'])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
 
+			if (strlen($username) < 3) {
+				echo "Sorry, your username needs to be longer than 3 characters.";
+			}
+		}
+
+
+
+	?>
+
+<form action="6.php" method="POST">
+	<label for="username">Username</label>
+	<input name="username" type="text" placeholder="Ex: Kaz123">
+	<br>
+	<label for="password">Password</label>
+	<input name="password" type="password">
+	<br>
+	<button name="submit" type="submit">Submit</button>
+</form>
 
 </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php" ?>
