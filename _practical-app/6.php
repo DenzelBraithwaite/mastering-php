a1ebcc0e1053d249465013b9867665d6722925aa
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-12-11 13:30:38 -0500

    added dummy file for form submission

--- _practical-app/6.php
+++ _practical-app/6.php
@@ -12,7 +12,19 @@
 
 
 <article class="main-content col-xs-8">
- 
+
+<form action="test_form.php" method="POST">
+  <label for="first-name">First Name:</label>
+  <input name="first-name" type="text" placeholder="Ex: John">
+  <br>
+  <label for="last-name">Last Name:</label>
+  <input name="last-name" type="text" placeholder="Ex: Smith">
+  <br>
+  <label for="password">Password:</label>
+  <input name="password" type="password">
+  <br>
+  <button name ="submit" type='submit'>Submit</button>
+</form>
 
 	<?php  
 
@@ -20,7 +32,6 @@
 
 
  */
-
 	
 ?>
 
